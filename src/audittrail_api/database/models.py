695144632766cb675b6a19d2a1a7d53ab01d408b
Victor Odoyo <victorodoyo555@gmail.com>
2026-08-29 12:08:29 +0300

    feat: persist audit export jobs

--- src/audittrail_api/database/models.py
+++ src/audittrail_api/database/models.py
@@ -2,7 +2,15 @@
 
 from audittrail_api.auth.models import APIKey
 from audittrail_api.events.models import AuditEvent
+from audittrail_api.exports.models import ExportJob
 from audittrail_api.organizations.models import Application, Organization
 from audittrail_api.retention.models import RetentionPolicy
 
-__all__ = ["APIKey", "Application", "AuditEvent", "Organization", "RetentionPolicy"]
+__all__ = [
+    "APIKey",
+    "Application",
+    "AuditEvent",
+    "ExportJob",
+    "Organization",
+    "RetentionPolicy",
+]
